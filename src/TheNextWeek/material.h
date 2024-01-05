8387095d193603df4e078497c1eeab80b72ec2b3
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2024-01-05 00:44:48 -0500

    Revert "revert TNW to IOW"
    This reverts commit cd52427b1c2434b5caf98dca96dbe12a229b33d5.

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -13,12 +13,17 @@
 
 #include "rtweekend.h"
 
-#include "hittable_list.h"
+#include "hittable.h"
+#include "texture.h"
 
 
 class material {
   public:
     virtual ~material() = default;
+
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0,0,0);
+    }
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
@@ -28,7 +33,8 @@ class material {
 
 class lambertian : public material {
   public:
-    lambertian(const color& a) : albedo(a) {}
+    lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
@@ -38,13 +44,13 @@ class lambertian : public material {
         if (scatter_direction.near_zero())
             scatter_direction = rec.normal;
 
-        scattered = ray(rec.p, scatter_direction);
-        attenuation = albedo;
+        scattered = ray(rec.p, scatter_direction, r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
         return true;
     }
 
   private:
-    color albedo;
+    shared_ptr<texture> albedo;
 };
 
 
@@ -55,7 +61,7 @@ class metal : public material {
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere(), r_in.time());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
@@ -87,7 +93,7 @@ class dielectric : public material {
         else
             direction = refract(unit_direction, rec.normal, refraction_ratio);
 
-        scattered = ray(rec.p, direction);
+        scattered = ray(rec.p, direction, r_in.time());
         return true;
     }
 
@@ -100,6 +106,42 @@ class dielectric : public material {
         r0 = r0*r0;
         return r0 + (1-r0)*pow((1 - cosine),5);
     }
+};
+
+
+class diffuse_light : public material {
+  public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        return false;
+    }
+
+    color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+  private:
+    shared_ptr<texture> emit;
+};
+
+
+class isotropic : public material {
+  public:
+    isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        scattered = ray(rec.p, random_unit_vector(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+
+  private:
+    shared_ptr<texture> albedo;
 };
 
 
